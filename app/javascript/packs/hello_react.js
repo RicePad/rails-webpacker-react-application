5ecaa6f0f74c5d4da5b5732cbf45b0178bd98b70
JON <jonn.thinkrtc@gmail.com>
2017-09-13 15:35:45 -0700

    first initial commit and config

---
+++ app/javascript/packs/hello_react.js
@@ -0,0 +1,12 @@
+// import React, { Component } from 'react';
+// import ReactDom from 'react-dom';
+// import App from '../components/App'
+// import { Router, Route, browserHistory } from 'react-router';
+//
+// ReactDom.render(
+// <Router path="/" history={browserHistory}>
+//   <Route path="/" component={App} />
+//
+// <Router> , document.querySelector(".app-name")
+//
+// )
